c8212d6e94181021edea1a636f14435a54d0baee
Juliya <yula_sakura@list.ru>
2023-09-04 22:46:17 +0300

    add_changes

--- src/App.js
+++ src/App.js
@@ -6,128 +6,161 @@ function App() {
     const graph = [[0, 1, 0], [0, 1, 0], [0, 0, 0]]
     const n = 3 //строки
     const m = 3 //столбцы
-    const start_i = 0;
     const start_j = 0;
+    const start_i = 0;
     const end_j = 2;
     const end_i = 0;
+
+    // const graph = [[0, 1, 0], [0, 1, 0], [0, 1, 0]]
+    // const n = 3 //строки
+    // const m = 3 //столбцы
+    // const start_j = 0;
+    // const start_i = 0;
+    // const end_j = 2;
+    // const end_i = 0;
+
+    // const graph = [[0, 0, 0], [0, 0, 0]]
+    // const n = 2 //строки
+    // const m = 3 //столбцы
+    // const start_j = 0;
+    // const start_i = 0;
+    // const end_j = 2;
+    // const end_i = 1;
 
     const blockElements = [];
     graph.map((a) => a.map((b) => b === 1 ? blockElements.push(b) : ''))
 
     function findMoveAlongXY (x, next_i, next_j, distances, distance, visitedCells, neighborValues, direction) {
-        let moveAlongY_start = (next_i === end_i) ? 'match' : (next_i < end_i);
-        let moveAlongX_start = (next_j === end_j) ? 'match' : (next_j < end_j);
+        let moveAlongY = (next_i === end_i) ? 'match' : (next_i < end_i);
+        let moveAlongX = (next_j === end_j) ? 'match' : (next_j < end_j);
 
-        let moveAlongX = moveAlongX_start;
-        let moveAlongY = moveAlongY_start;
+        console.log('XY')
 
-        if (visitedCells.length === (n * m - blockElements.length)) {
-            return Math.min(...distances)
-            console.log(Math.min(...distances), 'result')
-        } else if (((moveAlongX === 'match') && (moveAlongY === 'match')) || !direction) {
+        if ((visitedCells.length === (n * m - blockElements.length - 1)) || (distances.length === n * m - blockElements.length - 1)) {
             distances.push(distance)
-            distance = 0;
-            console.log(distances, 'match, push distance')
-            findMoveAlongXY(0, start_i, start_j, distances, distance, true)
+            const filtered_distances = distances.filter((a) => a !== 0)
+            if (filtered_distances.length !== 0) {
+                console.log(Math.min(...filtered_distances), 'result')
+            } else {
+                console.log(0, 'result')
+            }
+        } else if (((moveAlongX === 'match') && (moveAlongY === 'match')) || !direction) {
+            if ((moveAlongX === 'match') && (moveAlongY === 'match')) {
+                console.log(distances, 'match, push distance')
+                distances.push(distance)
+            } else {
+                console.log(distances, 'not match, push distance')
+                distances.push(0)
+            }
+            findMoveAlongXY(0, start_i, start_j, distances, 0, visitedCells, [],true)
         } else if ((moveAlongX === 'match') && (moveAlongY !== 'match')) {
+            console.log('matchX')
             moveAlongX = true;
             recursiveDisplacementCalculations(x, moveAlongX, moveAlongY, next_i, next_j, true, distances, distance, visitedCells, neighborValues);
         } else if ((moveAlongX !== 'match') && (moveAlongY === 'match')) {
+            console.log('matchY')
+            moveAlongY = true;
+            recursiveDisplacementCalculations(x, moveAlongX, moveAlongY, next_i, next_j, true, distances, distance, visitedCells, neighborValues);
+        } else {
+            console.log('not match')
             moveAlongY = true;
             recursiveDisplacementCalculations(x, moveAlongX, moveAlongY, next_i, next_j, true, distances, distance, visitedCells, neighborValues);
         }
-    }
 
-    function recursiveDisplacementCalculations (x, moveAlongX, moveAlongY, next_i, next_j, direction, distances, distance, visitedCells, neighborValues) {
-        if (direction !== true) {
-            moveAlongX = !moveAlongX;
-            moveAlongY = !moveAlongY;
-        }
-
-        const findSiblingValues = (directionX, directionY) => {
-            if (directionX === true) {
-                if ((next_j + 1) > m) {
-                    neighborValues[0] = 1;
-                } else {
-                    neighborValues[0] = graph[next_i][next_j + 1];
-                }
-            } else if (directionX === false) {
-                if ((next_j - 1) < 0) {
-                    neighborValues[0] = 1;
-                } else {
-                    neighborValues[0] = graph[next_i][next_j - 1];
-                }
-            }
-
-            if (directionY === true) {
-                if ((next_i + 1) > n) {
-                    neighborValues[1] = 1;
-                } else {
-                    neighborValues[1] = graph[next_i + 1][next_j];
-                }
-            } else if (directionY === false) {
-                if ((next_i - 1) < 0) {
-                    neighborValues[1] = 1;
-                } else {
-                    neighborValues[1] = graph[next_i - 1][next_j];
-                }
-            }
-            console.log(neighborValues, 'find siblings step 1')
-        }
-        findSiblingValues(moveAlongX, moveAlongY)
-        const changeCells = (ordinate, movement) => {
-            movement ? (ordinate === 'next_i' ? next_j += 1 : next_i += 1)
-                : (ordinate === 'next_i' ? next_j -= 1 : next_i -= 1);
-            distance += 1
-            if (visitedCells.filter((item) => item === [next_i, next_j]).length === 0) {
-                visitedCells.push([next_i, next_j])
-                console.log(visitedCells, visitedCells.slice(-1), visitedCells.slice(-2, -1), 'change cells')
-            }
-        }
-        console.log(x, moveAlongX, moveAlongY, 'step 2')
-
-        if (neighborValues[x] === 0) {
-            if (x === 0) {
-                if ((moveAlongX === true) && ((visitedCells.filter((item) => item === [next_i, next_j + 1]).length === 0) || (neighborValues[Number(!x)] !== 0)) && (visitedCells.length > 0 ? (visitedCells.slice(-1)[0][0] !== next_i && visitedCells.slice(-1)[0][1] !== (next_j + 1)) : true) && (visitedCells.length > 1 ? (visitedCells.slice(-2, -1)[0][0] !== next_i && visitedCells.slice(-2, -1)[0][1] !== (next_j + 1)) : true)) {
-                    changeCells('next_i', moveAlongX)
-                    console.log(next_i, next_j, visitedCells, distances, 'change x, true')
-                    findMoveAlongXY(Number(!x), next_i, next_j, distances, distance, visitedCells, neighborValues, true)
-                } else if ((moveAlongX === false) && ((visitedCells.filter((item) => item === [next_i, next_j - 1]).length === 0) || (neighborValues[Number(!x)] !== 0)) && (visitedCells.length > 0 ? (visitedCells.slice(-1)[0][0] !== next_i && visitedCells.slice(-1)[0][1] !== (next_j - 1)) : true) && (visitedCells.length > 1 ? (visitedCells.slice(-2, -1)[0][0] !== next_i && visitedCells.slice(-2, -1)[0][1] !== (next_j - 1)) : true)) {
-                    changeCells('next_i', moveAlongX)
-                    console.log(next_i, next_j, visitedCells, distances, 'change x, false')
-                    findMoveAlongXY(Number(!x), next_i, next_j, distances, distance, visitedCells, neighborValues, true)
-                } else {
-                    if(!!direction) {
-                        console.log('findWrongWay, x=0')
-                        recursiveDisplacementCalculations(0, moveAlongX, moveAlongY, next_i, next_j, false, distances, distance, visitedCells, neighborValues);
-                    }
-                }
-            } else if (x === 1) {
-                if ((moveAlongY === true) && ((visitedCells.filter((item) => item === [next_i + 1, next_j]).length === 0) || (neighborValues[x] !== 0)) && (visitedCells.length > 0 ? (visitedCells.slice(-1)[0][0] !== (next_i + 1) && visitedCells.slice(-1)[0][1] !== next_j) : true) && (visitedCells.length > 1 ? (visitedCells.slice(-2, -1)[0][0] !== (next_i + 1) && visitedCells.slice(-2, -1)[0][1] !== next_j) : true)) {
-                    changeCells('next_j', moveAlongY)
-                    console.log(next_i, next_j, visitedCells, distances, 'change y, true')
-                    findMoveAlongXY(0, next_i, next_j, distances, distance, visitedCells, neighborValues, true)
-                } else if ((moveAlongY === false) && ((visitedCells.filter((item) => item === [next_i - 1, next_j]).length === 0) || (neighborValues[x] !== 0)) && (visitedCells.length > 0 ? (visitedCells.slice(-1)[0][0] !== (next_i - 1) && visitedCells.slice(-1)[0][1] !== next_j) : true) && (visitedCells.length > 1 ? (visitedCells.slice(-2, -1)[0][0] !== (next_i - 1) && visitedCells.slice(-2, -1)[0][1] !== next_j) : true)) {
-                    changeCells('next_j', moveAlongY)
-                    console.log(next_i, next_j, visitedCells, distances, 'change y, false')
-                    findMoveAlongXY(0, next_i, next_j, distances, distance, visitedCells, neighborValues, true)
-                } else {
-                    if (!!direction) {
-                        console.log('findWrongWay, x=1')
-                        return recursiveDisplacementCalculations(0, moveAlongX, moveAlongY, next_i, next_j, false, distances, distance, visitedCells, neighborValues);
-                    }
-                }
-            }
-        } else if (neighborValues[Number(!x)] === 0) {
-            console.log('step 3, recursive !x')
-            x = Number(!x);
-            recursiveDisplacementCalculations(x, moveAlongX, moveAlongY, next_i, next_j, true, distances, distance, visitedCells, neighborValues);
-        } else {
+        function recursiveDisplacementCalculations(x, moveAlongX, moveAlongY, next_i, next_j, direction, distances, distance, visitedCells, neighborValues) {
             if (!direction) {
-                findMoveAlongXY(0, next_i, next_j, distances, distance, visitedCells, neighborValues, false)
+                moveAlongX = !moveAlongX;
+                moveAlongY = !moveAlongY;
+            }
+
+            console.log(moveAlongX, moveAlongY, 'moveAlongX, moveAlongY')
+
+            const findSiblingValues = (directionX, directionY) => {
+                if (directionX) {
+                    if (next_j === (m - 1)) {
+                        neighborValues[0] = 1;
+                    } else {
+                        neighborValues[0] = graph[next_i][next_j + 1];
+                    }
+                } else if (!directionX) {
+                    if ((next_j - 1) < 0) {
+                        neighborValues[0] = 1;
+                    } else {
+                        neighborValues[0] = graph[next_i][next_j - 1];
+                    }
+                }
+
+                if (directionY) {
+                    if (next_i === (n - 1)) {
+                        neighborValues[1] = 1;
+                    } else {
+                        console.log(next_i + 1, next_j, graph)
+                        neighborValues[1] = graph[next_i + 1][next_j];
+                    }
+                } else if (!directionY) {
+                    if ((next_i - 1) < 0) {
+                        neighborValues[1] = 1;
+                    } else {
+                        neighborValues[1] = graph[next_i - 1][next_j];
+                    }
+                }
+                console.log(neighborValues, 'find siblings step 1')
+            }
+            findSiblingValues(moveAlongX, moveAlongY)
+            const changeCells = (ordinate, movement) => {
+                movement ? (ordinate === 'next_i' ? next_j += 1 : next_i += 1)
+                    : (ordinate === 'next_i' ? next_j -= 1 : next_i -= 1);
+                distance = distance + 1
+                if (visitedCells.filter((item) => item === [next_i, next_j]).length === 0) {
+                    visitedCells.push([next_i, next_j])
+                    console.log(distance, visitedCells, visitedCells.slice(-1), visitedCells.slice(-2, -1), 'change cells')
+                }
+            }
+            console.log(x, moveAlongX, moveAlongY, visitedCells, 'step 2')
+
+            if (neighborValues[x] === 0) {
+                if (x === 0) {
+                    if ((moveAlongX === true) && (((visitedCells.length > 0) && ((next_j + 1) <= (m - 1))) ? (visitedCells.slice(-1)[0][1] !== (next_j + 1)) : true) && (visitedCells.length > 1 ? (visitedCells.slice(-2, -1)[0][1] !== (next_j + 1)) : true)) {
+                        changeCells('next_i', moveAlongX)
+                        console.log(next_i, next_j, visitedCells, distances, 'change x, true')
+                        findMoveAlongXY(1, next_i, next_j, distances, distance, visitedCells, [], true)
+                    } else if ((moveAlongX === false) && (((visitedCells.length > 0) && ((next_j - 1) > 0)) ? (visitedCells.slice(-1)[0][0] !== next_i && visitedCells.slice(-1)[0][1] !== (next_j - 1)) : true) && (visitedCells.length > 1 ? (visitedCells.slice(-2, -1)[0][1] !== (next_j - 1)) : true)) {
+                        changeCells('next_i', moveAlongX)
+                        console.log(next_i, next_j, visitedCells, distances, 'change x, false')
+                        findMoveAlongXY(1, next_i, next_j, distances, distance, visitedCells, [], true)
+                    } else {
+                        if (!!direction) {
+                            console.log('findWrongWay, x=0')
+                            recursiveDisplacementCalculations(0, moveAlongX, moveAlongY, next_i, next_j, false, distances, distance, visitedCells, neighborValues);
+                        }
+                    }
+                } else if (x === 1) {
+                    if ((moveAlongY === true) && (((visitedCells.length > 0) && ((next_i + 1) <= (n - 1))) ? (visitedCells.slice(-1)[0][0] !== (next_i + 1)) : true) && (visitedCells.length > 1 ? (visitedCells.slice(-2, -1)[0][0] !== (next_i + 1)) : true)) {
+                        changeCells('next_j', moveAlongY)
+                        console.log(next_i, next_j, visitedCells, distances, 'change y, true')
+                        findMoveAlongXY(0, next_i, next_j, distances, distance, visitedCells, [], true)
+                    } else if ((moveAlongY === false) && (((visitedCells.length > 0) && ((next_i - 1) > 0)) ? (visitedCells.slice(-1)[0][0] !== (next_i - 1)) : true) && (visitedCells.length > 1 ? (visitedCells.slice(-2, -1)[0][0] !== (next_i - 1)) : true)) {
+                        changeCells('next_j', moveAlongY)
+                        console.log(next_i, next_j, visitedCells, distances, 'change y, false')
+                        findMoveAlongXY(0, next_i, next_j, distances, distance, visitedCells, [], true)
+                    } else {
+                        if (!!direction) {
+                            console.log('findWrongWay, x=1')
+                            return recursiveDisplacementCalculations(0, moveAlongX, moveAlongY, next_i, next_j, false, distances, distance, visitedCells, neighborValues);
+                        }
+                    }
+                }
+            } else if (neighborValues[Number(!x)] === 0) {
+                console.log('step 3, recursive !x')
+                x = Number(!x);
+                recursiveDisplacementCalculations(x, moveAlongX, moveAlongY, next_i, next_j, true, distances, distance, visitedCells, neighborValues);
             } else {
-                console.log('findWrongWay')
-                recursiveDisplacementCalculations(0, moveAlongX, moveAlongY, next_i, next_j, false, distances, distance, visitedCells, neighborValues);
+                if (!direction) {
+                    findMoveAlongXY(0, next_i, next_j, distances, distance, visitedCells, [], false)
+                } else {
+                    console.log('findWrongWay')
+                    recursiveDisplacementCalculations(0, moveAlongX, moveAlongY, next_i, next_j, false, distances, distance, visitedCells, neighborValues);
+                }
             }
         }
     }
@@ -141,87 +174,3 @@ function App() {
 }
 
 export default App;
-
-
-// // Функция для нахождения кратчайшего пути в лабиринте
-// function findShortestPath(N, M, x1, y1, x2, y2, labyrinth) {
-//     // Создаем двумерный массив для хранения расстояний до каждой ячейки
-//     let distances = [];
-//     for (let i = 0; i < N; i++) {
-//         distances[i] = [];
-//         for (let j = 0; j < M; j++) {
-//             distances[i][j] = Infinity;
-//         }
-//     }
-//
-//     // Начальное расстояние до точки пробуждения будет 0
-//     distances[x1][y1] = 0;
-//
-//     // Создаем очередь, которую будем использовать для обхода лабиринта в ширину
-//     let queue = [];
-//     queue.push({ x: x1, y: y1 });
-//
-//     // Пока очередь не пуста, продолжаем обходить лабиринт
-//     while (queue.length > 0) {
-//         let current = queue.shift();
-//
-//         // Проверяем ячейки, куда можно переместиться из текущей ячейки
-//         let neighbors = [
-//             { x: current.x + 1, y: current.y }, // справа
-//             { x: current.x - 1, y: current.y }, // слева
-//             { x: current.x, y: current.y + 1 }, // сверху
-//             { x: current.x, y: current.y - 1 }, // снизу
-//         ];
-//
-//         for (let i = 0; i < neighbors.length; i++) {
-//             let neighbor = neighbors[i];
-//
-//             // Проверяем, что соседняя ячейка находится в пределах лабиринта
-//             if (neighbor.x >= 0 && neighbor.x < N && neighbor.y >= 0 && neighbor.y < M) {
-//                 // Проверяем, что соседняя ячейка является проходом (0) и еще не была посещена
-//                 if (labyrinth[neighbor.x][neighbor.y] === 0 && distances[neighbor.x][neighbor.y] === Infinity) {
-//                     // Обновляем расстояние до соседней ячейки
-//                     distances[neighbor.x][neighbor.y] = distances[current.x][current.y] + 1;
-//
-//                     // Если соседняя ячейка - точка выхода, возвращаем найденное расстояние
-//                     if (neighbor.x === x2 && neighbor.y === y2) {
-//                         return distances[neighbor.x][neighbor.y];
-//                     }
-//
-//                     // Добавляем соседнюю ячейку в очередь для дальнейшего обхода
-//                     queue.push(neighbor);
-//                 }
-//             }
-//         }
-//     }
-//
-//     // Если не удалось достичь точки выхода, возвращаем 0
-//     return 0;
-// }
-//
-// // Чтение входных данных
-// let input = require('fs').readFileSync('/dev/stdin', 'utf8');
-// let lines = input.split('\n');
-//
-// let NM = lines[0].split(' ').map(Number);
-// let N = NM[0];
-// let M = NM[1];
-//
-// let start = lines[1].split(' ').map(Number);
-// let x1 = start[0];
-// let y1 = start[1];
-//
-// let end = lines[2].split(' ').map(Number);
-// let x2 = end[0];
-// let y2 = end[1];
-//
-// let labyrinth = [];
-// for (var i = 3; i < N + 3; i++) {
-//     labyrinth.push(lines[i].split(' ').map(Number));
-// }
-//
-// // Вызов функции для нахождения кратчайшего пути
-// let shortestPathLength = findShortestPath(N, M, x1, y1, x2, y2, labyrinth);
-//
-// // Вывод результатов
-// console.log(shortestPathLength);
